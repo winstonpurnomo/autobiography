b6f2f132865a354ca89f0b717f2265bdabf25317
Winston Purnomo <winstonpurnomo@gmail.com>
2025-02-11 19:46:04 -0800

    fix

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,8 +3,5 @@
 export function Providers({ children }: { children: React.ReactNode }) {
   // const queryClient = new QueryClient();
 
-  return (
-    // <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    { children }
-  );
+  return <>{children}</>;
 }
